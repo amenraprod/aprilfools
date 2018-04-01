c1c8bec397dcb271509b5c0d541d6068048e7710
amenraprod <amenraprod@gmail.com>
2018-04-01 19:59:26 -0400

    added test route to find user #1

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function() {
+  return App\User::find(1)->profile;
+})
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
