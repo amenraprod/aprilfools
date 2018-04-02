3b44dbb2bdaaf01cee49d7018a621559d9b87816
amenraprod <amenraprod@gmail.com>
2018-04-02 04:35:32 -0400

    added profiles table

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+
+      <div class="panel panel-default">
+            <div class="panel-heading">
+                  Edit profile
+            </div>
+
+            <div class="panel-body">
+                  <form action="{{ route('user.store') }}" method="post">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                              <label for="name">User</label>
+                              <input type="text" name="name" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                              <label for="name">New Password</label>
+                              <input type="email" name="password" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                              <label for="name">Upload a new avatar</label>
+                              <input type="file" name="avatar" class="form-control">
+                        </div>
+                        <div class="form-group">
+                              <label for="name">Gender</label>
+                              <input type="text" name="gender" class="form-control">
+                        </div>
+                        <div class="form-group">
+                              <label for="name">DOB</label>
+                              <input type="text" name="dob" class="form-control">
+                        </div>
+                        <div class="form-group">
+                              <label for="name">Phone Number</label>
+                              <input type="text" name="phonenumber" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                              <div class="text-center">
+                                    <button class="btn btn-success" type="submit">
+                                          Add user
+                                    </button>
+                              </div>
+                        </div>
+                  </form>
+            </div>
+      </div>
+@stop
