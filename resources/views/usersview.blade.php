5d8f446c28f62d361f810095a9a8974204d74182
amenraprod <amenraprod@gmail.com>
2018-04-01 23:17:05 -0400

    fixed view

--- resources/views/usersview.blade.php
+++ resources/views/usersview.blade.php
@@ -24,12 +24,34 @@
                         </thead>
 
                         <tbody>
-
-                                        
-
+                              @if($users->count() > 0)
+                                    @foreach($users as $user)
+                                          <tr>
+                                                <td>
+                                                      <img src="{{ asset($user->profile->avatar) }}" alt="" width="60px" height="60px" style="border-radius: 50%;">
+                                                </td>
+                                                <td>
+                                                      {{ $user->name }}
+                                                </td>
+                                                <td>
+                                                      @if($user->admin)
+                                                             <class="btn btn-xs btn-danger">Remove permissions</a>
+                                                      @else
+                                                             <class="btn btn-xs btn-success">Make admin
+                                                      @endif
+                                                </td>
+                                                <td>
+                                                      @if(Auth::id() !== $user->id)
+                                                             <class="btn btn-xs btn-danger">Delete
+                                                      @endif
+                                                </td>
+                                          </tr>
+                                    @endforeach
+                              @else
+                                    <tr>
                                           <th colspan="5" class="text-center">No users</th>
                                     </tr>
-
+                              @endif
                         </tbody>
                   </table>
             </div>
